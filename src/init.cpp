0ec67e96c31fa0669635f725ed450cecb2bef1a8
Brannon King <countprimes@gmail.com>
2020-01-27 17:38:38 -0700

    fixed slow loading of block index, increased wallet flush period

--- src/init.cpp
+++ src/init.cpp
@@ -1752,7 +1752,6 @@ bool AppInitMain(InitInterfaces& interfaces)
     //// debug print
     {
         LOCK(cs_main);
-        LogPrintf("block tree size = %u\n", ::BlockIndex().size());
         chain_active_height = ::ChainActive().Height();
     }
     LogPrintf("nBestHeight = %d\n", chain_active_height);
